72eecfd0f1c37f8e1c58977ec06150d6ad5f1261
r-sie <32829606+Arsiela@users.noreply.github.com>
2024-09-23 17:21:23 +0800

    09232024 - Added functionality for Service Mechanic
    09232024 - Added functionality for Service Mechanic

---
+++ src/test/java/ServiceMechanicTest.java
@@ -0,0 +1,238 @@
+
+import org.guanzon.appdriver.base.GRider;
+import org.guanzon.auto.main.clients.Service_Mechanic;
+import org.json.simple.JSONObject;
+import org.junit.AfterClass;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Arsiela
+ */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class ServiceMechanicTest {
+    static Service_Mechanic model;
+    JSONObject json;
+    boolean result;
+    
+    public ServiceMechanicTest(){}
+    
+    @BeforeClass
+    public static void setUpClass() {   
+        
+        String path;
+        if(System.getProperty("os.name").toLowerCase().contains("win")){
+            path = "D:/GGC_Maven_Systems";
+        }
+        else{
+            path = "/srv/GGC_Maven_Systems";
+        }
+        System.setProperty("sys.default.path.config", path);
+        GRider instance = new GRider("gRider");
+        if (!instance.logUser("gRider", "M001000001")){
+            System.err.println(instance.getMessage() + instance.getErrMsg());
+            System.exit(1);
+        }
+        
+        System.out.println("Connected");
+        System.setProperty("sys.default.path.metadata", "D:/GGC_Maven_Systems/config/metadata/");
+        
+        
+        JSONObject json;
+        
+        System.out.println("sBranch code = " + instance.getBranchCode());
+        model = new Service_Mechanic(instance,false, instance.getBranchCode());
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+        
+    }
+    
+    /**
+     * COMMENTED TESTING TO CLEAN AND BUILD PROPERLY
+     * WHEN YOU WANT TO CHECK KINDLY UNCOMMENT THE TESTING CASES (@Test).
+     *  - ARSIELA 
+     */
+    
+//    @Test
+//    public void test01NewRecord() {
+//        System.out.println("--------------------------------------------------------------------");
+//        System.out.println("------------------------------NEW RECORD--------------------------------------");
+//        System.out.println("--------------------------------------------------------------------");
+//        
+//        json = model.newRecord();
+//        if ("success".equals((String) json.get("result"))){
+//            json = model.getModel().getModel().setClientID("A00118000001");
+//            if ("error".equals((String) json.get("result"))){
+//                System.err.println((String) json.get("message"));
+//                System.exit(1);
+//            }
+//            json = model.getModel().getModel().setBrpSkill("y");
+//            if ("error".equals((String) json.get("result"))){
+//                System.err.println((String) json.get("message"));
+//                System.exit(1);
+//            }
+//            json = model.getModel().getModel().setTchSkill("y");
+//            if ("error".equals((String) json.get("result"))){
+//                System.err.println((String) json.get("message"));
+//                System.exit(1);
+//            }
+//        } else {
+//            System.err.println("result = " + (String) json.get("result"));
+//            fail((String) json.get("message"));
+//        }
+//        
+//    }
+//    
+//    @Test
+//    public void test01NewRecordSave(){
+//        System.out.println("--------------------------------------------------------------------");
+//        System.out.println("------------------------------NEW RECORD SAVING--------------------------------------");
+//        System.out.println("--------------------------------------------------------------------");
+//        
+//        json = model.saveRecord();
+//        System.err.println((String) json.get("message"));
+//        
+//        if (!"success".equals((String) json.get("result"))){
+//            System.err.println((String) json.get("message"));
+//            result = false;
+//        } else {
+//            System.out.println((String) json.get("message"));
+//            result = true;
+//        }
+//        assertTrue(result);
+//    }
+    
+//    @Test
+//    public void test02OpenRecord(){
+//        System.out.println("--------------------------------------------------------------------");
+//        System.out.println("------------------------------RETRIEVAL--------------------------------------");
+//        System.out.println("--------------------------------------------------------------------");
+//        
+//        json = model.openRecord("A00118000001");
+//        
+//        if (!"success".equals((String) json.get("result"))){
+//            result = false;
+//        } else {
+//            System.out.println("--------------------------------------------------------------------");
+//            System.out.println("SERVICE MECHANIC INFORMATION");
+//            System.out.println("--------------------------------------------------------------------");
+//            System.out.println("sClientID  :  " + model.getModel().getModel().getClientID());
+//            System.out.println("cRecdStat  :  " + model.getModel().getModel().getRecdStat()); 
+//            System.out.println("sLastName  :  " + model.getModel().getModel().getLastName()); 
+//            System.out.println("sFrstName  :  " + model.getModel().getModel().getFrstName()); 
+//            System.out.println("sMiddName  :  " + model.getModel().getModel().getMiddName()); 
+//            System.out.println("sCompnyNm  :  " + model.getModel().getModel().getCompnyNm()); 
+//            System.out.println("cClientTp  :  " + model.getModel().getModel().getClientTp()); 
+//            System.out.println("sAddressx  :  " + model.getModel().getModel().getAddress());
+//            
+//            result = true;
+//        }
+//        assertTrue(result);
+//    }
+//    
+//    @Test
+//    public void test03UpdateRecord(){
+//        System.out.println("--------------------------------------------------------------------");
+//        System.out.println("------------------------------UPDATE RECORD--------------------------------------");
+//        System.out.println("--------------------------------------------------------------------");
+//        
+//        json = model.updateRecord();
+//        System.err.println((String) json.get("message"));
+//        if ("error".equals((String) json.get("result"))){
+//            System.err.println((String) json.get("message"));
+//            result = false;
+//        } else {
+//            result = true;
+//        }
+//        
+//        json = model.getModel().getModel().setBrpSkill("y");
+//        if ("error".equals((String) json.get("result"))){
+//            System.err.println((String) json.get("message"));
+//            System.exit(1);
+//        }
+//        json = model.getModel().getModel().setTchSkill("n");
+//        if ("error".equals((String) json.get("result"))){
+//            System.err.println((String) json.get("message"));
+//            System.exit(1);
+//        }
+//        
+//        assertTrue(result);
+//        //assertFalse(result);
+//    }
+//    
+//    @Test
+//    public void test03UpdateRecordSave(){
+//        System.out.println("--------------------------------------------------------------------");
+//        System.out.println("------------------------------UPDATE RECORD SAVING--------------------------------------");
+//        System.out.println("--------------------------------------------------------------------");
+//        
+//        json = model.saveRecord();
+//        System.err.println((String) json.get("message"));
+//        
+//        if (!"success".equals((String) json.get("result"))){
+//            System.err.println((String) json.get("message"));
+//            result = false;
+//        } else {
+//            System.out.println((String) json.get("message"));
+//            result = true;
+//        }
+//        assertTrue(result);
+//        //assertFalse(result);
+//    }
+    
+        
+//    @Test
+//    public void test04DeactivateRecord(){
+//        System.out.println("--------------------------------------------------------------------");
+//        System.out.println("------------------------------DEACTIVATE RECORD--------------------------------------");
+//        System.out.println("--------------------------------------------------------------------");
+//        
+//        json = model.deactivateRecord("M001000001");
+//        System.err.println((String) json.get("message"));
+//        
+//        if (!"success".equals((String) json.get("result"))){
+//            System.err.println((String) json.get("message"));
+//            result = false;
+//        } else {
+//            System.out.println((String) json.get("message"));
+//            result = true;
+//        }
+//        
+//        assertTrue(result);
+//        //assertFalse(result);
+//    }
+//    
+//    @Test
+//    public void test05ActivateRecord(){
+//        System.out.println("--------------------------------------------------------------------");
+//        System.out.println("------------------------------ACTIVATE RECORD--------------------------------------");
+//        System.out.println("--------------------------------------------------------------------");
+//        
+//        json = model.activateRecord("A00118000001");
+//        System.err.println((String) json.get("message"));
+//        
+//        if (!"success".equals((String) json.get("result"))){
+//            System.err.println((String) json.get("message"));
+//            result = false;
+//        } else {
+//            System.out.println((String) json.get("message"));
+//            result = true;
+//        }
+//        
+//        assertTrue(result);
+//        //assertFalse(result);
+//    }
+}
