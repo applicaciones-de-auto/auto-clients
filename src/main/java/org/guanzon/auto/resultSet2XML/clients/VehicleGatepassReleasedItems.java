26bdc2434f5445e552a47bff178acfa49ba1725a
r-sie <32829606+Arsiela@users.noreply.github.com>
2024-10-25 13:38:02 +0800

    10252024 - Update xml of Vehicle Gatepass released items
    10252024 - Update xml of Vehicle Gatepass released items

--- src/main/java/org/guanzon/auto/resultSet2XML/clients/VehicleGatepassReleasedItems.java
+++ src/main/java/org/guanzon/auto/resultSet2XML/clients/VehicleGatepassReleasedItems.java
@@ -48,7 +48,7 @@ public class VehicleGatepassReleasedItems {
                         + "  , a.nReleased "                                     
                         + "  , b.sLaborDsc "                                     
                         + "  , c.sDescript AS sStockDsc "                         
-                        + "  , 'xxxxxxxxxxxx' AS sDSNoxxxx "                                  
+                        + "  , 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' AS sDSNoxxxx "          //Can be multiple Job Order                        
                         + " FROM vehicle_released_items a "                      
                         + " LEFT JOIN labor b ON b.sLaborCde = a.sItemCode "     
                         + " LEFT JOIN inventory c ON c.sStockIDx = a.sItemCode "
